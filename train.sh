3784ae5a1473ba8ec9aa0395f40eeb379ae45eb4
Stephen Mayhew <swm.mayhew@gmail.com>
2019-06-13 18:55:51 -0400

    updates

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 
-
+B=gs://uig_bert
 
 python run_pretraining.py \
-       --input_file=/tmp/*.tfrecord \
-       --output_dir=/tmp/pretraining_output \
+       --input_file=$B/data/*.tfrecord \
+       --output_dir=$B/pretraining_output \
        --do_train=True \
        --do_eval=True \
-       --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+       --bert_config_file=$B/bert_config.json \
        --train_batch_size=32 \
        --max_seq_length=128 \
-       --use_tpue = True \
+       --use_tpu = True \
        --max_predictions_per_seq=20 \
        --num_train_steps=10000 \
        --num_warmup_steps=10 \
